085985fed9b999f85a0f741901420aec554c51b8
Frank Silva <frankalves333@gmail.com>
2022-10-02 20:50:46 -0300

    Controle Financeiro em construção

---
+++ index.js
@@ -0,0 +1,120 @@
+const item = document.querySelector("#entrada");
+const itensGasto = document.querySelector("#itemGasto")
+const ganho = document.querySelector("#valorGanho");
+const gasto = document.querySelector("#valorGasto");
+const btnAdc = document.querySelector("#BtnAdicionar");
+const bntAdcValor = document.querySelector("#btnAdicionarValor");
+const bntAdcGasto = document.querySelector("#BtnAdicionarGasto");
+const lista = item.value;
+const listaValor = document.querySelector("#listaValor");
+const listaGasto = document.querySelector("#listaGastos")
+const listaSaida = document.querySelector("#listaSaida")
+
+btnAdc.addEventListener("click", (e) => {
+  e.preventDefault();
+  const itemGanho = item.value;
+
+  console.log(itemGanho);
+  adicionarLista();
+});
+
+bntAdcGasto.addEventListener("click", (e) => {
+  e.preventDefault();
+  const itensGastos = itensGasto.value;
+
+  console.log(itensGastos);
+  adicionarGastos();
+});
+
+// bntAdcValor.addEventListener("click", (e) => {
+//   e.preventDefault();
+
+//   const valorGasto = gasto.value;
+//   console.log(valorGasto);
+//   adicionarPreco();
+// });
+
+function adicionarLista() {
+  li = document.createElement("li");
+  itens = item.value;
+
+  li.textContent = "Item:" + " " + itens[0].toUpperCase() + itens.substring(1);
+
+  // select the ul menu element
+  const listaItem = document.querySelector("#listaItem");
+  listaItem.appendChild(li);
+
+  li = document.createElement("li");
+  li.style.backgroundColor = "green";
+  
+  li.textContent = "Valor: " + "R$" + " " + ganho.value;
+
+  // select the ul menu element
+  const listaValor = document.querySelector("#listaValor");
+  listaItem.appendChild(li);
+  valorFinal();
+}
+
+function adicionarGastos() {
+  li = document.createElement("li");
+  itens = itensGasto.value;
+
+  li.textContent = "Item:" + " " + itens[0].toUpperCase() + itens.substring(1);
+
+  // select the ul menu element
+  const listaGastos = document.querySelector("#listaGastos");
+  listaGastos.appendChild(li);
+
+  li = document.createElement("li");
+  li.style.backgroundColor = "red";
+  
+  li.textContent = "Valor: " + "R$" + " " + gasto.value;
+
+  // select the ul menu element
+  const listaSaidas = document.querySelector("#listaSaida");
+  listaGastos.appendChild(li);
+  valorFinal();
+}
+
+
+function valorFinal() {
+  const total = document.querySelector(".total");
+  let totalGanho = [ganho.value];
+  let totalGasto = [gasto.value];
+
+  for (let i = 0; i < totalGanho.length; i++) {
+    total.innerHTML = "Total: " + totalGanho[i] + totalGasto[i];
+      
+  }
+}
+
+
+
+// function testeApi() {
+//   fetch(`https://jsonplaceholder.typicode.com/todos`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       data.map((item) => {
+//         li = document.createElement("li");
+
+//         li.textContent = item.id;
+//         const listaItem = document.querySelector("#listaItem");
+//         listaItem.appendChild(li);
+//         li = document.createElement("li");
+
+//         li.textContent = item.title;
+//         const listaValor = document.querySelector("#listaValor");
+//         listaValor.appendChild(li);
+
+//         console.log(item);
+//       });
+//     });
+// }
+
+//  cabelo 2.400
+//  oculos 600
+//  camisa 3.000
+//  calça 3.000
+//  tenis 2.400
+//  luva 600
+//  asa 900
